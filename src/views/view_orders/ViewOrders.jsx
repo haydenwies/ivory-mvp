a501b44cbf9ce0754cfd72b89be2c459cc7f6b58
Hayden Wies <haydenwies@gmail.com>
2022-05-31 15:49:26 -0400

    Added functionality to ReceiptData page and ViewOrder page.

--- src/views/view_orders/ViewOrders.jsx
+++ src/views/view_orders/ViewOrders.jsx
@@ -1,8 +1,15 @@
-import React from "react";
 import "./viewOrder.css";
 import FinancialInfo from "./FinancialInfo";
 import ReceiptData from "./ReceiptData";
+import { useState } from "react";
+
 function ViewOrders() {
+  const [showReceiptData, setShowReceiptData] = useState(true);
+
+  const onToggle = () => {
+    setShowReceiptData(!showReceiptData)
+  }
+
   return (
     <div className="view-orders row-c-c">
       <div className="view-order-container col-fs-c">
@@ -10,14 +17,24 @@ function ViewOrders() {
         <div className="view-order-header">
           <h1>View Orders</h1>
           <div className="nav-tabs row-fs-c">
-            <h4 className={false ? "active-tab" : ""}>Receipt Data</h4>{" "}
-            <h4 className={true ? "active-tab" : ""}>Financial Info</h4>
+            <h4 
+              className={showReceiptData ? "active-tab" : ""} 
+              onClick={onToggle}
+            >
+              Receipt Data
+            </h4>
+            <h4 
+              className={!showReceiptData ? "active-tab" : ""}
+              onClick={onToggle}
+            >
+              Financial Info
+            </h4>
           </div>
         </div>
         {/* ----------------------------- Receipt Section ----------------------------- */}
-        {true && <ReceiptData />}
+        {showReceiptData && <ReceiptData />}
         {/* ----------------------------- Financial Section ----------------------------- */}
-        {/* {true&& <FinancialInfo />} */}
+        {!showReceiptData && <FinancialInfo />}
       </div>
     </div>
   );
