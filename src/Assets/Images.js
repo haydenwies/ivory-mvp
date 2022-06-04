dc4d84692e81b598ae4bfc043683a9ce0d1bc9ef
KevinYuCode <76442800+KevinYuCode@users.noreply.github.com>
2022-06-04 12:54:15 -0400

    Completed most of the order functionality

--- src/Assets/Images.js
+++ src/Assets/Images.js
@@ -14,6 +14,7 @@ import Saved from "./Saved.svg";
 import Logout from "./Logout.svg";
 import Blur from "./Blur.svg";
 import AddIcon from "./AddIcon.svg";
+import CheckMark from "./CheckMark.svg";
 
 export {
   WhiteLogo,
@@ -32,4 +33,5 @@ export {
   Logout,
   Blur,
   AddIcon,
+  CheckMark
 };
